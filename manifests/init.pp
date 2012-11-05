f4eeb50ff195acd12faeb5bafe81552e151a634e
Dieter De Meyer <dieterdemeyer@gmail.com>
2012-11-05 18:34:28 +0100

    Initial draft version of proftpd module

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,5 +12,39 @@
 #
 class proftpd {
 
+  package { 'proftpd' :
+    ensure => present
+  }
+
+  file { '/etc/proftpd.conf':
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    content => template("${module_name}/proftpd.erb"),
+    require => Package['proftpd']
+  }
+
+  service { 'proftpd':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    subscribe  => File['/etc/proftpd.conf']
+  }
+
+  user { 'proftpd':
+    ensure     => present,
+    uid        => '101',
+    gid        => 'proftpd',
+    shell      => '/bin/sh',
+    comment    => 'ProFTPd user',
+    home       => '/var/run/proftpd',
+    managehome => false,
+    require    => Group['proftpd']
+  }
+
+  group { 'proftpd':
+    ensure => present,
+    gid    => '1001'
+  }
 
 }
