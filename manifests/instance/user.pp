4196b6ee2669135e509eb59db23bebb921de7463
fdammeke <fabian.dammekens@cegeka.be>
2013-04-10 15:25:23 +0200

    Style fix http://puppet-lint.com/checks/variables_not_enclosed/

--- manifests/instance/user.pp
+++ manifests/instance/user.pp
@@ -1,4 +1,14 @@
-define proftpd::instance::user($ipaddress=undef, $port=undef, $username=undef, $password=undef, $uid=undef, $gid=undef, $comment='', $home="/home/$username", $ensure = present) {
+define proftpd::instance::user(
+  $ipaddress=undef,
+  $port=undef,
+  $username=undef,
+  $password=undef,
+  $uid=undef,
+  $gid=undef,
+  $comment='',
+  $home="/home/${username}",
+  $ensure = present
+) {
 
   include proftpd
 
@@ -25,54 +35,54 @@ define proftpd::instance::user($ipaddress=undef, $port=undef, $username=undef, $
   case $ensure_real {
     'absent':
       {
-        Augeas <| title == "$vhost_name.passwd/$username/rm" |>
+        Augeas <| title == "${vhost_name}.passwd/${username}/rm" |>
       }
     'present':
       {
-        augeas { "$vhost_name.passwd/$username/add" :
+        augeas { "${vhost_name}.passwd/${username}/add" :
           lens    => 'Passwd.lns',
           incl    => "/etc/proftpd/users.d/${vhost_name}.passwd",
           context => "/files/etc/proftpd/users.d/${vhost_name}.passwd",
           changes => [
-            "set [last()+1] $username",
-            "set $username/password $password",
-            "set $username/uid $uid_real",
-            "set $username/gid $gid_real",
-            "set $username/name '$comment'",
-            "set $username/home $home",
-            "set $username/shell /bin/false"
+            "set [last()+1] ${username}",
+            "set ${username}/password ${passwora}d",
+            "set ${username}/uid ${uid_real}",
+            "set ${username}/gid ${gid_real}",
+            "set ${username}/name '${comment}'",
+            "set ${username}/home ${home}",
+            "set ${username}/shell /bin/false"
           ],
-          onlyif  => "match $username size == 0",
+          onlyif  => "match ${username} size == 0",
           require => Class['proftpd']
         }
 
-        augeas { "$vhost_name.passwd/$username/modify" :
+        augeas { "${vhost_name}.passwd/${username}/modify" :
           lens    => 'Passwd.lns',
           incl    => "/etc/proftpd/users.d/${vhost_name}.passwd",
           context => "/files/etc/proftpd/users.d/${vhost_name}.passwd",
           changes => [
-            "set $username/password $password",
-            "set $username/uid $uid_real",
-            "set $username/gid $gid_real",
-            "set $username/name '$comment'",
-            "set $username/home $home",
-            "set $username/shell /bin/false"
+            "set ${username}/password ${password}",
+            "set ${username}/uid ${uid_real}",
+            "set ${username}/gid ${gid_real}",
+            "set ${username}/name '${comment}'",
+            "set ${username}/home ${home}",
+            "set ${username}/shell /bin/false"
           ],
-          onlyif  => "match $username size == 1",
+          onlyif  => "match ${username} size == 1",
           require => Class['proftpd']
         }
       }
     default: { notice('The given ensure parameter is not supported') }
   }
 
-  @augeas { "$vhost_name.passwd/$username/rm" :
+  @augeas { "${vhost_name}.passwd/${username}/rm" :
     lens    => 'Passwd.lns',
     incl    => "/etc/proftpd/users.d/${vhost_name}.passwd",
     context => "/files/etc/proftpd/users.d/${vhost_name}.passwd",
     changes => [
-      "rm $username",
+      "rm ${username}",
     ],
-    onlyif  => "match $username size > 0",
+    onlyif  => "match ${username} size > 0",
     require => Class['proftpd']
   }
 
