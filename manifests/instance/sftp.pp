0936cb77940f10b7c321d31745cff8bfc14bac8e
fdammeke <fabian.dammekens@cegeka.be>
2013-04-10 14:58:45 +0200

    Initial sftp instance configuration

--- manifests/instance/sftp.pp
+++ manifests/instance/sftp.pp
@@ -1,2 +1,49 @@
-define proftpd::instance::sftp($ipaddress='0.0.0.0', $port='22', $logdir=undef) {
+define proftpd::instance::sftp(
+  $ensure=present,
+  $ipaddress='0.0.0.0',
+  $port='22',
+  $server_name='sFTP server',
+  $server_ident='sFTP server ready',
+  $server_admin='root@server',
+  $logdir=undef,
+  $max_clients='45',
+  $max_loginattempts='3',
+  $default_root='~',
+  $allowoverride='on',
+) {
+
+  include proftpd
+
+  if ($logdir == undef) {
+    fail("Proftpd::Instance::Ftp[${title}]: parameter logdir must be defined")
+  }
+
+  $vhost_name = "${ipaddress}_${port}"
+
+  file { "/etc/proftpd/sites.d/${vhost_name}.conf":
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/sites.d/ftp.conf.erb"),
+    notify  => Service['proftpd']
+  }
+
+  file { "/etc/proftpd/users.d/${vhost_name}.conf":
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/users.d/users.conf.erb"),
+    notify  => Service['proftpd']
+  }
+
+  file { "/etc/proftpd/users.d/${vhost_name}.passwd":
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    replace => false
+  }
+
 }
