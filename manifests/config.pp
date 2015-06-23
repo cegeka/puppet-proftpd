a00ebe5193bfab2395553f4c4f77ef5a1585f933
Dieter De Meyer <dieterdemeyer@gmail.com>
2015-06-23 13:35:46 +0200

    Redesign module to use package/config/service pattern

---
+++ manifests/config.pp
@@ -0,0 +1,54 @@
+class proftpd::config (
+  $manage_proftpd_conf = false
+) {
+
+  file { '/etc/proftpd':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+  }
+
+  file { [ '/etc/proftpd/sites.d', '/etc/proftpd/users.d' ]:
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/etc/proftpd']
+  }
+
+  file { '/etc/proftpd.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/proftpd.conf.erb"),
+    replace => $manage_proftpd_conf
+  }
+
+  file { '/etc/proftpd/modules.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/modules.conf.erb"),
+    require => File['/etc/proftpd']
+  }
+
+  user { 'proftpd':
+    ensure     => present,
+    uid        => '5001',
+    gid        => 'proftpd',
+    shell      => '/bin/sh',
+    comment    => 'ProFTPd user',
+    home       => '/var/run/proftpd',
+    managehome => false,
+    require    => Group['proftpd']
+  }
+
+  group { 'proftpd':
+    ensure => present,
+    gid    => '5001'
+  }
+
+}
---
+++ manifests/service.pp
@@ -0,0 +1,25 @@
+class proftpd::service (
+  $ensure = 'running',
+  $enable = true
+) {
+
+  validate_re($ensure, '^running$|^stopped$|^unmanaged$')
+
+  case $ensure {
+    'running', 'stopped': {
+      service { 'proftpd':
+        ensure     => $ensure,
+        enable     => $enable,
+        hasstatus  => true,
+        hasrestart => true
+      }
+    }
+    'unmanaged': {
+      notice('Class[proftpd::service]: service is currently not being managed')
+    }
+    default: {
+      fail('Class[proftpd::service]: parameter ensure must be running, stopped or unmanaged')
+    }
+  }
+
+}
