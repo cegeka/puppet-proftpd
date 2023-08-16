18f3db100cd68cfe14c01fa4cb90e9d9adf87b0a
Fabian Dammekens <fabian@dammekens.be>
2023-08-16 15:45:54 +0200

    [PROFTP] Update database.sql examples file for future use

--- examples/database.sql
+++ examples/database.sql
@@ -1,27 +1,6 @@
--- Host: localhost
--- Generation Time: Aug 29, 2013 at 09:57 AM
--- Server version: 5.0.77
--- PHP Version: 5.3.3
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
--- Database: `ftpmysql`
+-- Create table ftplog, logs can be stored here
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ftplog`
---
-
 CREATE TABLE IF NOT EXISTS `ftplog` (
   `id` int(11) NOT NULL auto_increment,
   `timestamp` datetime NOT NULL,
@@ -40,72 +19,72 @@ CREATE TABLE IF NOT EXISTS `ftplog` (
   KEY `username` (`username`),
   KEY `protocol` (`protocol`),
   KEY `command` (`command`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=96728577 ;
-
--- --------------------------------------------------------
+);
 
 --
--- Table structure for table `ftp_group`
+-- Create table ftp_group, groups can be stored here
 --
-
 CREATE TABLE IF NOT EXISTS `ftp_group` (
   `groupname` varchar(16) NOT NULL default '',
-  `gid` int(7) NOT NULL default '65534',
+  `gid` int(7) NOT NULL default 65534,
   `members` varchar(16) NOT NULL default '',
-  KEY `groupname` (`groupname`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='ProFTP group table';
-
--- --------------------------------------------------------
+  PRIMARY KEY (`groupname`),
+  UNIQUE KEY `groupname` (`groupname`)
+);
 
 --
--- Table structure for table `ftp_users`
+-- Create table ftp_users, users can be stored here
 --
-
 CREATE TABLE IF NOT EXISTS `ftp_users` (
   `id` int(11) NOT NULL auto_increment,
-  `customer` int(11) NOT NULL,
+  `customer` int(11) NOT NULL default 1,
   `userid` varchar(255) NOT NULL,
   `realname` varchar(32) NOT NULL default '',
   `shell` varchar(20) NOT NULL default '/sbin/nologin',
-  `passwd` varchar(255) NOT NULL,
+  `passwd` varchar(255) NOT NULL default '!!',
   `active` char(1) NOT NULL default 'Y',
   `uid` int(11) NOT NULL default '101',
   `gid` int(11) NOT NULL default '65534',
   `home` varchar(255) NOT NULL default '/home/catchall',
   `lastchange` varchar(50) NOT NULL default '',
   `deleted` varchar(1) NOT NULL default 'N',
-  `delchange` datetime NOT NULL,
-  `ftp` varchar(10) NOT NULL default 'true',
+  `delchange` datetime NOT NULL default CURRENT_TIMESTAMP,
+  `ftp` varchar(10) NOT NULL default 'false',
   `sftp` varchar(10) NOT NULL default 'true',
-  `ftps` varchar(10) NOT NULL default 'true',
-  `migrated` tinyint(1) NOT NULL,
+  `ftps` varchar(10) NOT NULL default 'false',
+  `migrated` tinyint(1) NOT NULL default 1,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `userid` (`userid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=556 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sftp_hostkeys`
---
+);
 
 CREATE TABLE IF NOT EXISTS `sftp_hostkeys` (
   `host` varchar(255) NOT NULL,
   `sshkey` varchar(255) NOT NULL,
   KEY `sftphostkeys_idx` (`host`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+);
 
 --
--- Table structure for table `sftp_userkeys`
+-- Table for sFTP user keys, similar to authorized_key
 --
-
 CREATE TABLE IF NOT EXISTS `sftp_userkeys` (
   `id` int(11) NOT NULL auto_increment,
   `customer` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `sshkey` text NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `sftpuserkeys_idx` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=87 ;
+);
+
+--
+-- Create the proftp group with group id 65534
+--
+INSERT INTO `ftp_group` (
+  groupname, gid
+  ) VALUES ('proftp',65534);
+
+--
+-- Create a dummy user with SHA-512 encoded password
+--  Most values will default to the table defaults, edit where needed!
+--  Use this for demonstration purpose only, don't put this in production!
+--
+INSERT INTO `ftp_users` (userid,realname,passwd) values ('dummy','My first dummy user','$6$uCpTpX0HWrDjF14d$FMFS73l1C/.5jc3gtve5Px3AGUAZ0mssvmJ6kpvSk3xAhXZU8D1qopr4EoyHhIlucGdrHAPGzYUkJ2v0FqHrF/');
